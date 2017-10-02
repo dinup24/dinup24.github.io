1d3b09561d3fa53a6f9d91d790c984b3d13e8218
DINUP PADMANABHA PILLAI <dinup.pillai@in.ibm.com>
2017-10-02 23:57:23 +0530

    message

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$('.nav-item').on('click',function(e){
+   e.preventDefault();
+
+  //Remove any previous active classes
+  $('.nav-item').removeClass('active');
+
+  //Add active class to the clicked item
+  $(this).addClass('active');
+  
+  $('.section').removeClass('visible');
+  
+  $($(this).find('.nav-link').attr('href')).addClass('visible');
+});
+
+/*$(function(){
+  $(".navbar-nav a").click(function(e){
+    e.preventDefault();
+    $('html,body').scrollTo(this.hash,this.hash); 
+  });
+});*/
+
+$(document).ready(function () {
+    $("nav").find("li").on("click", "a", function () {
+        $('.navbar-collapse.in').collapse('hide');
+    });
+});
+
+$(function(){ 
+     var navMain = $("#navbarsExampleDefault");
+     navMain.on("click", "a", null, function () {
+         navMain.collapse('hide');
+     });
+ });
+
