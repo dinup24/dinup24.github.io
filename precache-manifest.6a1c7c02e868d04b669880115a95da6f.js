882fda0b3f0decc43d67e348d1650a0ade467b6f
dinup-pillai <dinup.pillai@in.ibm.com>
2020-05-25 13:24:22 +0530

    changing about us

---
+++ precache-manifest.6a1c7c02e868d04b669880115a95da6f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b68177a78e07613e4308dc35d606f27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de141efdb9dd8e67a177",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "493bee71c8d083f20128",
+    "url": "/static/js/2.4d469275.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4d469275.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de141efdb9dd8e67a177",
+    "url": "/static/js/main.5f21696c.chunk.js"
+  },
+  {
+    "revision": "4af3bc0d051c657efa57",
+    "url": "/static/js/runtime-main.ef2b5ab2.js"
+  },
+  {
+    "revision": "beb6a6f7cfef1d5e8f4979dc5fc9de92",
+    "url": "/static/media/accounting-finance-department.beb6a6f7.jpg"
+  },
+  {
+    "revision": "d93881a253b35dcbd69445f71e0efbba",
+    "url": "/static/media/accounting_banner.d93881a2.jpg"
+  },
+  {
+    "revision": "e4801d8058f0e065895de483071ae2ff",
+    "url": "/static/media/banner.e4801d80.png"
+  },
+  {
+    "revision": "aaca7a7cf565350e8aae1638ee9de4cf",
+    "url": "/static/media/ca_alex.aaca7a7c.jpg"
+  },
+  {
+    "revision": "62db9106a67d75dcf6a8596c7de2f58e",
+    "url": "/static/media/ca_james.62db9106.jpg"
+  },
+  {
+    "revision": "ad1945b97fc78925aba0157ba106a720",
+    "url": "/static/media/ca_murali.ad1945b9.jpg"
+  },
+  {
+    "revision": "89c703b0589ac88661b4649239baa20f",
+    "url": "/static/media/ca_padmanabha_pillai.89c703b0.jpg"
+  },
+  {
+    "revision": "666d948481914d620827bf709f2dfcfa",
+    "url": "/static/media/ca_venkat.666d9484.jpg"
+  },
+  {
+    "revision": "7f695bd030e9b9017f8a08a0e434116d",
+    "url": "/static/media/logo.7f695bd0.png"
+  }
+]);
